10b6f26cc3e1fb5793a6f83ac713e7241dd471fd
jmyebras <jmyebras@hotmail.com>
2016-02-23 23:11:26 +0100

    Versión 2

--- 16_ejemplo_git/src/principal/Test.java
+++ 16_ejemplo_git/src/principal/Test.java
@@ -5,8 +5,10 @@ public class Test {
 	public static void main(String[] args) {
 		int a = 1;
 		int b = 2;
-		System.out.println("Ejemplo de uso de GIT - versión 1");
+		int c = a + b;
+		System.out.println("Ejemplo de uso de GIT - versión 2");
 		System.out.println("Variable a: " + a);
 		System.out.println("Variable b: " + b);
+		System.out.println("Variable c: " + c);
 	}
 }
