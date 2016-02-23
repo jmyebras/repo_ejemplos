aa355436d3fa34d9633aa1448765e95bdec1edb8
jmyebras <jmyebras@hotmail.com>
2016-02-23 23:12:30 +0100

    Versión 3

--- 16_ejemplo_git/src/principal/Test.java
+++ 16_ejemplo_git/src/principal/Test.java
@@ -6,9 +6,9 @@ public class Test {
 		int a = 1;
 		int b = 2;
 		int c = a + b;
-		System.out.println("Ejemplo de uso de GIT - versión 2");
-		System.out.println("Variable a: " + a);
-		System.out.println("Variable b: " + b);
-		System.out.println("Variable c: " + c);
+		System.out.println("Ejemplo de uso de GIT - versión 5");
+		System.out.println("Variable a: " + a + ", variable b: " + b + ", variable c: " + c);
+		System.out.println("Variable c: " + c + ", variable b: " + b + ", variable a: " + a);
+		System.out.println("Fin del programa");
 	}
 }
